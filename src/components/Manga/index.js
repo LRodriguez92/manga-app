7f5d3cd6dbb9c72305114defaf83dcaef3d8f329
student_123 <student_123@admins-MacBook-Air-5.local>
2018-11-17 13:14:58 -0500

    modified functions to use json files

--- src/components/Manga/index.js
+++ src/components/Manga/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import mangaDetails from '../../mangaDetails';
 
 export default function Manga(props) {
   let i = -1;
   return (
   <div>
+    <h1>hi</h1>
     {props.mangaList.map(manga => {
       i++;
-       return <h1 key={i}>{manga.title}</h1>
+       return (
+         <div key={i}>
+           <h1>{manga}</h1>
+           <img src={`${manga.imageURL}`}/>
+           <p>{manga.description}</p>
+         </div>
+       )
     })}
   </div>
   )
--- src/components/MangaList/index.js
+++ src/components/MangaList/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Manga from '../Manga';
+import mangaDetails from '../../mangaDetails';
+
 
 export default function MangaList(props) {
+  let i = -1;
   return (
     <div>
-      <Manga mangaList={props.mangaList}/>
+      {props.mangaList.map(string => {
+        i++;
+         return (
+           <div key={i}>
+             <h1>{string}</h1>
+             <img src={`${string.imageURL}`}/>
+             <p>{string.description}</p>
+           </div>
+         )
+      })}
+      {/* <Manga mangaList={props.mangaList}/> */}
     </div>
   )
 }
