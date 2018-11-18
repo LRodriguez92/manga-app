8acfd5d51309a5c1bf8ea49ddca62a35b07b1477
student_123 <student_123@admins-Air-5.home>
2018-11-18 11:26:29 -0500

    added first set of styling

--- src/components/Manga/index.js
+++ src/components/Manga/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import mangaDetails from '../../mangaDetails';
+import './index.css';
 
 
 export default function Manga(props) {
   const manga = mangaDetails.manga;
   let key = -1;
   return (
-    <div className="manga">
+    <div id="mangas">
       {props.mangaList.map(name => {
         key++;
         for(let i = 0; i < manga.length; i++) {
           if (name === manga[i].title) {
             return (
               <div className="container" key={key}>
-                <h1>{manga[i].title}</h1>
-                <div className="body">
-                  <img src={manga[i].imageURL}/>
-                  <div className="details">
-                    <p><strong>Released:</strong> {manga[i].released}</p>
-                    <p><strong>Author:</strong> {manga[i].author}</p>
-                    <p><strong>Artist:</strong> {manga[i].artist}</p>
-                    <p dangerouslySetInnerHTML={{__html: manga[i].description}}/>
+                <div className="card">
+                  <h1>{manga[i].title}</h1>
+                    <div className="body">
+                      <img src={manga[i].imageURL}/>
+                      <div className="details">
+                        <p><strong>Released:</strong> {manga[i].released}</p>
+                        <p><strong>Author:</strong> {manga[i].author}</p>
+                        <p><strong>Artist:</strong> {manga[i].artist}</p>
+                        <div className="description">
+                          <p dangerouslySetInnerHTML={{__html: manga[i].description}}/>
+                        </div>
+                    </div>
                   </div>
                 </div>
               </div>
